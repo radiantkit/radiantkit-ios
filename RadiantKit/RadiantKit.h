5b7e4eb28bc640bb87511f074adbeed3b86a4d2c
Simon Strandgaard <neoneye@gmail.com>
2019-06-15 15:55:19 +0200

    A minimal Xcode framework for iOS

---
+++ RadiantKit/RadiantKit.h
@@ -0,0 +1,19 @@
+//
+//  RadiantKit.h
+//  RadiantKit
+//
+//  Created by Simon Strandgaard on 15/06/2019.
+//  Copyright © 2019 Simon Strandgaard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RadiantKit.
+FOUNDATION_EXPORT double RadiantKitVersionNumber;
+
+//! Project version string for RadiantKit.
+FOUNDATION_EXPORT const unsigned char RadiantKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RadiantKit/PublicHeader.h>
+
+
